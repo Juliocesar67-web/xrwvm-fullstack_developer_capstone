b623d6bed4eccb0e130f7db3b31e207d8092a207
Júlio Figueiredo <juliofigueiredo08@gmail.com>
2026-02-25 13:20:52 -0100

    module 3

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,12 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
 
-# CarModelAdmin class
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "car_make", "type", "year")
+
+
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
 
 # CarMakeAdmin class with CarModelInline
 
